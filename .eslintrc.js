f60fab1415ff2acdd5f64530196eebe7d5afd2a8
inlinger <inlinger@outlook.com>
2022-03-24 14:35:33 +0800

    feat: 应用eslint规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,10 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
+    '@oit/eslint-config',
   ],
   parserOptions: {
     parser: '@babel/eslint-parser',
   },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-};
+  rules: {},
+}
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+      For a guide and recipes on how to configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
@@ -38,7 +38,7 @@ export default {
   props: {
     msg: String,
   },
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
